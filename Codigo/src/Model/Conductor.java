0a0fe9af542d438821c5f7081fb43d8dce5898c6
Laura-sofia-fi <laura.carvajal3@estudiantesunibague.edu.co>
2026-08-22 14:29:43 -0500

    Inicio del CRUD y creacion de clases principales

---
+++ Codigo/src/Model/Conductor.java
@@ -0,0 +1,24 @@
+package Model;
+
+public class Conductor extends Usuario {
+    private int licencia;
+
+    public Conductor(String estado, String contraseña,
+                     String telefono, String correo,
+                     String nombre, int idUsuario, int licencia) {
+        super(estado, contraseña, telefono, correo, nombre, idUsuario);
+        this.licencia = licencia;
+    }
+
+    public Conductor(int licencia) {
+        this.licencia = licencia;
+    }
+
+    public int getLicencia() {
+        return licencia;
+    }
+
+    public void setLicencia(int licencia) {
+        this.licencia = licencia;
+    }
+}
---
+++ Codigo/src/Model/Pasajero.java
@@ -0,0 +1,4 @@
+package Model;
+
+public class Pasajero extends Usuario{
+}
