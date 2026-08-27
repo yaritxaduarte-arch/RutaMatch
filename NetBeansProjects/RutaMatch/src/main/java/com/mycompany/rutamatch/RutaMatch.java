ba1cdea3a1627d0648fd4e5e97bc8cb3a9590f39
Laura-sofia-fi <laura.carvajal3@estudiantesunibague.edu.co>
2026-08-27 15:32:51 -0500

    Creación del CRUD de pasajero y prueba en rutamacht.java

--- NetBeansProjects/RutaMatch/src/main/java/com/mycompany/rutamatch/RutaMatch.java
+++ NetBeansProjects/RutaMatch/src/main/java/com/mycompany/rutamatch/RutaMatch.java
@@ -2,6 +2,7 @@ package com.mycompany.rutamatch;
 
 import com.mycompany.rutamatch.controller.UsuarioController;
 import com.mycompany.rutamatch.modelo.Pasajero;
+import com.mycompany.rutamatch.modelo.Usuario;
 import com.mycompany.rutamatch.view.GUIPrincipal;
 
 public class RutaMatch {
@@ -33,6 +34,42 @@ public class RutaMatch {
                     "Usuario encontrado: " + encontrado.getNombre()
             );
         }
+
+Pasajero pasajeroActualizado = new Pasajero(
+        "Activo",
+        "5678",
+        "3119876543",
+        "laura.nueva@gmail.com",
+        "Laura Actualizada",
+        1
+);
+
+controller.actualizarUsuario(pasajeroActualizado);
+
+System.out.println("Usuario actualizado correctamente.");
+
+Pasajero actualizado = (Pasajero) controller.buscarUsuario(1);
+
+System.out.println(
+        "Nombre despues de actualizar: "
+        + actualizado.getNombre()
+);
+
+System.out.println(
+        "Correo despues de actualizar: "
+        + actualizado.getCorreo()
+);
+
+controller.eliminarUsuario(1);
+
+System.out.println("Usuario eliminado correctamente.");
+
+Usuario eliminado = controller.buscarUsuario(1);
+
+if (eliminado == null) {
+    System.out.println("El usuario ya no existe.");
+}
+
 
 
         GUIPrincipal gui = new GUIPrincipal();
--- NetBeansProjects/RutaMatch/src/main/java/com/mycompany/rutamatch/modelo/Pasajero.java
+++ NetBeansProjects/RutaMatch/src/main/java/com/mycompany/rutamatch/modelo/Pasajero.java
@@ -1,5 +1,11 @@
 package com.mycompany.rutamatch.modelo;
 
-public class Pasajero   extends Usuario{
-    
+public class Pasajero extends Usuario {
+
+    public Pasajero(String estado, String contraseña,
+                    String telefono, String correo,
+                    String nombre, int idUsuario) {
+
+        super(estado, contraseña, telefono, correo, nombre, idUsuario);
+    }
 }
