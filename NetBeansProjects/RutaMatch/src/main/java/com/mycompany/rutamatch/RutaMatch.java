dd38d43e21f8462ce6aa8622acaa24cab9b94067
Laura-sofia-fi <laura.carvajal3@estudiantesunibague.edu.co>
2026-08-27 15:25:25 -0500

    Prueba del CRUD de pasajero

--- NetBeansProjects/RutaMatch/src/main/java/com/mycompany/rutamatch/RutaMatch.java
+++ NetBeansProjects/RutaMatch/src/main/java/com/mycompany/rutamatch/RutaMatch.java
@@ -1,19 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.rutamatch;
+
+import com.mycompany.rutamatch.controller.UsuarioController;
+import com.mycompany.rutamatch.modelo.Pasajero;
 import com.mycompany.rutamatch.view.GUIPrincipal;
-/**
- *
- * @author yarit
- */
+
 public class RutaMatch {
 
     public static void main(String[] args) {
-       System.out.println("Hello World!");
+
+        
+        UsuarioController controller = new UsuarioController();
+
+       
+        Pasajero pasajero = new Pasajero(
+                "Activo",
+                "1234",
+                "3001234567",
+                "laura@gmail.com",
+                "Laura",
+                1
+        );
+
+
+        controller.crearUsuario(pasajero);
+
+        System.out.println("Usuario creado correctamente.");
+
+        Pasajero encontrado = (Pasajero) controller.buscarUsuario(1);
+
+        if (encontrado != null) {
+            System.out.println(
+                    "Usuario encontrado: " + encontrado.getNombre()
+            );
+        }
+
 
         GUIPrincipal gui = new GUIPrincipal();
         gui.setVisible(true);
     }
 }
+
+
